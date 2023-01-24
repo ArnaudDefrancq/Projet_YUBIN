1097ae439fe28babd4d6c779cac22bfe5cc0f54e
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2023-01-24 10:41:49 +0100

    Ajout des headers

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,21 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // synchronise les tables
-// models.sequelize.sync({ alter: true });
+models.sequelize.sync({ alter: true });
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 // Vérif connection server
 app.get("/", (req, res) => {
