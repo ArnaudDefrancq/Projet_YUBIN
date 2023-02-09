5c24aee916db1f0906fba3c6485f5368d86e15e7
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2023-02-09 16:24:57 +0100

    Ajout du style partie feed

---
+++ frontend/src/components/newFeed/Feed.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Feed = () => {
+  return (
+    <div className="feed-container">
+      <div className="feed">
+        <div className="top-feed">
+          <img src="./images/mee.png" alt="avatar" className="feed-avatar" />
+          <div className="top-container">
+            <h2>Arnaud Defrancq</h2>
+            <p>
+              <i>Il y a 2j</i>
+            </p>
+          </div>
+        </div>
+        <div className="main-feed">
+          <p className="feed-content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error,
+            consequuntur repellendus natus tempora voluptatem soluta illo
+            reprehenderit ratione numquam voluptatibus!
+          </p>
+          <img src="./images/Logo-02.png" alt="" className="feed-image" />
+        </div>
+        <div className="footer-feed"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
--- frontend/src/pages/NewsFeed.tsx
+++ frontend/src/pages/NewsFeed.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import Navigation from '../components/Navigation';
-import AddFeed from '../components/newFeed/AddFeed';
+import React from "react";
+import Navigation from "../components/Navigation";
+import AddFeed from "../components/newFeed/AddFeed";
+import Feed from "../components/newFeed/Feed";
 
 const NewsFeed = () => {
-    return (
-        <>
-            <header>
-                <Navigation />
-            </header>
-            <main>
-                <AddFeed />
-            </main>
-        </>
-    );
+  return (
+    <>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <AddFeed />
+        <Feed />
+      </main>
+    </>
+  );
 };
 
 export default NewsFeed;
